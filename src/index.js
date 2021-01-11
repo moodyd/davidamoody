d9eac2e132f5f6e8e1169f5ea0790aeb98d35797
moodyd <moody@Davids-MBP.lan>
2021-01-11 17:18:47 -0500

    changed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './components/app/app.js';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
