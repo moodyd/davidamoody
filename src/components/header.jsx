52e7959aecea94f514997d7f9ba12abd09d5de1a
moodyd <moody@Davids-MBP.lan>
2021-01-12 20:55:44 -0500

    added animation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './app/app.css';
 
+const refreshPage = () =>{
+    window.location.reload();
+}
 
 const header = (<header>
     <div className="row align-items-centers">
         <div className="col">
-            <h2>David Armstrong Moody</h2>
+            <h2 onClick={refreshPage}>David Armstrong Moody</h2>
         </div>
     </div>
 </header>);
+
+
 
 class Header extends React.Component {
     render() {
